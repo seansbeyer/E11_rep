a6df5a234272e53eff49f7be05d4bfdd04767e28
seansbeyer <seansbeyer@gmail.com>
2025-02-05 16:15:21 -0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import adafruit_bme680
 import time
 import board
+import datetime
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()   # uses board.SCL and board.SDA
@@ -10,12 +11,16 @@ bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 bme680.sea_level_pressure = 1013.25
 
 counter = 0
-print(now.time())
+
+print(time.ctime())
+
+
 while counter < 10:
-    print("\nTemperature: %0.1f C" % bme680.temperature)
-    print("Gas: %d ohm" % bme680.gas)
-    print("Humidity: %0.1f %%" % bme680.relative_humidity)
-    print("Pressure: %0.3f hPa" % bme680.pressure)
-    print("Altitude = %0.2f meters" % bme680.altitude)
+    a = "Temperature: %0.1f C" % bme680.temperature
+    b = "Gas: %d ohm" % bme680.gas
+    c = "Humidity: %0.1f %%" % bme680.relative_humidity
+    d = "Pressure: %0.3f hPa" % bme680.pressure
+    e = "Altitude = %0.2f meters" % bme680.altitude
+    print(a + b + c + d + e)
     counter += 1
     time.sleep(2)
